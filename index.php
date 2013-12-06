985e1f765644dcfac579a82fa180aecbd83d2796
blackthought286 <blackthought286@gmail.com>
2013-12-05 22:19:47 -0500

    this is the first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<Doctype html>
+    <html>
+        <head>
+            <title></title>
+        </head>
+        <body>
+            <?php hello(); ?>
+        </body>
+
+    </html>
+
+
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Omega
+ * Date: 12/4/13
+ * Time: 4:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+   function hello(){
+       echo("hello");
+   }
+
+
+?>
